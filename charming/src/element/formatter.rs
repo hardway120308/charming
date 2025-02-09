410ae9d649f661dcb40329bce5640e6d6e8820d9
hardway <hardway120308@gmail.com>
2025-02-09 13:53:53 +0800

    feat: make string formatter available

--- charming/src/element/formatter.rs
+++ charming/src/element/formatter.rs
@@ -1,12 +1,64 @@
+use super::RawString;
 use serde::Serialize;
 
-use super::RawString;
+#[derive(Serialize)]
+#[serde(transparent)]
+pub struct FormatterFunction {
+    #[cfg(not(target_arch = "wasm32"))]
+    pub value: RawString,
+    #[cfg(target_arch = "wasm32")]
+    #[serde(with = "serde_wasm_bindgen::preserve")]
+    pub value: web_sys::js_sys::Function,
+}
 
-#[derive(Serialize, Debug, PartialEq, PartialOrd, Clone)]
+impl FormatterFunction {
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn new_no_args(body: &str) -> FormatterFunction {
+        FormatterFunction {
+            value: RawString::from(format!("function() {{ {} }}", body)),
+        }
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    pub fn new_no_args(body: &str) -> FormatterFunction {
+        FormatterFunction {
+            value: web_sys::js_sys::Function::new_no_args(body),
+        }
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn new_with_args(args: &str, body: &str) -> FormatterFunction {
+        FormatterFunction {
+            value: RawString::from(format!("function({}) {{ {} }}", args, body)),
+        }
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    pub fn new_with_args(args: &str, body: &str) -> FormatterFunction {
+        FormatterFunction {
+            value: web_sys::js_sys::Function::new_with_args(args, body),
+        }
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+
+impl<S> From<S> for FormatterFunction
+where
+    S: Into<String>,
+{
+    fn from(s: S) -> Self {
+        FormatterFunction {
+            value: RawString::from(s),
+        }
+    }
+}
+
+#[derive(Serialize)]
 #[serde(untagged)]
 pub enum Formatter {
     String(String),
-    Function(RawString),
+    Function(FormatterFunction),
 }
 
 impl From<&str> for Formatter {
@@ -15,8 +67,22 @@ impl From<&str> for Formatter {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 impl From<RawString> for Formatter {
     fn from(s: RawString) -> Self {
-        Formatter::Function(s)
+        Formatter::Function(FormatterFunction { value: s })
+    }
+}
+
+impl From<FormatterFunction> for Formatter {
+    fn from(f: FormatterFunction) -> Self {
+        Formatter::Function(f)
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+impl From<web_sys::js_sys::Function> for FormatterFunction {
+    fn from(f: web_sys::js_sys::Function) -> Self {
+        FormatterFunction { value: f }
     }
 }
